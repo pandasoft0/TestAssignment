71c87a3da69bca7a7b7fc056e1d6bcca13ef095a
Jonathan Speagle <speagledev@gmail.com>
2018-07-12 20:29:22 +0300

    Prefer to show only titles due to content is too big

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -3,26 +3,14 @@ import React, { PureComponent } from "react";
 import "./styles.css";
 
 export default class Post extends PureComponent {
-  createContent = () => {
-    const {
-      content: { rendered }
-    } = this.props.post;
-
-    return {
-      __html: rendered
-    };
-  };
-
   render() {
-    const { title, id } = this.props.post;
+    const { title, link } = this.props.post;
 
     return (
       <div className="post-container">
-        <div className="post-title">{title.rendered}</div>
-        <div
-          className="post-content"
-          dangerouslySetInnerHTML={this.createContent()}
-        />
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          <div className="post-title">{title.rendered}</div>
+        </a>
       </div>
     );
   }
